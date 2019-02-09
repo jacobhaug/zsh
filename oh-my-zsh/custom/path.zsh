9c17411a7d0c6681a6e7cd606a38c4cf87445c5d
Jacob Haug <jacob@kimmel.com>
2019-02-08 21:44:36 -0500

    updating js paths

--- oh-my-zsh/custom/path.zsh
+++ oh-my-zsh/custom/path.zsh
@@ -1,14 +1,10 @@
 # Node / Composer / RVM
-#export PATH="/usr/local/bin:/usr/local/sbin:/Users/jacob/.npm-global/bin:/Users/jacob/.composer/vendor/bin:/Users/jacob/.rvm/bin:$PATH"
 export PATH="/usr/local/bin:/usr/local/sbin:/Users/jacob/.composer/vendor/bin:/Users/jacob/.rvm/bin:$PATH"
-#export NVM_DIR="$HOME/.nvm"
-#source "/usr/local/opt/nvm/nvm.sh"
+export PATH="$HOME/.nodenv/bin:$PATH"
+eval "$(nodenv init -)"
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/jacob/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/jacob/google-cloud-sdk/path.zsh.inc'; fi
 
 # The next line enables shell command completion for gcloud.
 if [ -f '/Users/jacob/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/jacob/google-cloud-sdk/completion.zsh.inc'; fi
-
-#export PATH="$HOME/.nodenv/bin:$PATH"
-#eval "$(nodenv init -)"
